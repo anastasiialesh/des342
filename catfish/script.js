ad6e2b4327814adbd7ec3d892e665378b9e503e3
anastasiialesh <122308292+anastasiialesh@users.noreply.github.com>
2023-05-04 11:28:08 -0700

    added page 4

--- catfish/script.js
+++ catfish/script.js
@@ -1,12 +1,5 @@
 $('#text-3-b').click(function(){
-    $('#text-3-a').toggleClass('hide')
-    $('#text-3-b').toggleClass('hide')
-    $('#text-4-a').toggleClass('hide')
-})
-
-$('#text-4-a').click(function(){
-    $('#text-4-b').toggleClass('hide')
-})
-$('#text-4-b').click(function(){
-    $('#text-4-c').toggleClass('hide')
+    $('#text-3-a').addClass('hide')
+    $('#text-3-b').addClass('hide')
+    $('#text-4-a').removeClass('hide')
 })
