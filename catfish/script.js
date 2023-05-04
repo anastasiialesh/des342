cfb12f8ea31be7cfcc4738b3edabef1b70ea60e0
anastasiialesh <122308292+anastasiialesh@users.noreply.github.com>
2023-05-03 23:36:44 -0700

    working on toggle function

---
+++ catfish/script.js
@@ -0,0 +1,11 @@
+$('#page-3').click(function(){
+    $('#page-3').toggleClass('hide')
+    $('#text-4-a').toggleClass('hide')
+})
+
+$('#text-4-a').click(function(){
+    $('#text-4-b').toggleClass('hide')
+})
+$('#text-4-b').click(function(){
+    $('#text-4-c').toggleClass('hide')
+})
