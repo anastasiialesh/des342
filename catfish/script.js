972e9725c05fb9214fe5b336dac686e47f10b0f4
anastasiialesh <122308292+anastasiialesh@users.noreply.github.com>
2023-05-10 22:54:42 -0700

    added sound

--- catfish/script.js
+++ catfish/script.js
@@ -92,33 +92,7 @@ document.addEventListener('DOMContentLoaded', function () {
     goToTopOnLast: false,
     slideSelector: 'section'
   })
-  fps.onslide = function (e) {
-    if (e.target.currentSlide + 1 != 3) {
-      $('#text-3-a').removeClass('hide')
-      $('#text-3-b').removeClass('hide')
-      $('#text-4-a').addClass('hide')
-      $('#text-4-b').addClass('hide')
-      $('#text-4-b-2').addClass('hide')
-      $('#text-4-c').addClass('hide')
-    }
-    if (e.target.currentSlide + 1 == 3) {
-      setTimeout(() => {
-        $('#text-3-a').addClass('hide')
-        $('#text-3-b').addClass('hide')
-        $('#text-4-a').removeClass('hide')
-      }, '6000')
-      setTimeout(() => {
-        $('#text-4-b').removeClass('hide')
-      }, '8000')
-      setTimeout(() => {
-        $('#text-4-b-2').removeClass('hide')
-      }, '10000')
-      setTimeout(() => {
-        $('#text-4-c').removeClass('hide')
-      }, '12000')
-    }
-    console.log('Slide ' + (e.target.currentSlide + 1) + ' of ' + e.target.slides.length)
-  }
+  
   document.querySelectorAll('a.top').forEach(function (el) {
     el.addEventListener('click', function (event) {
       event.preventDefault()
@@ -197,3 +171,30 @@ $('#text-13-a').click(function() {
 $('#text-13-b').click(function() {
   $(this).toggleClass('pulse'); 
 });
+
+
+$('#text-15-a').click(function() {
+  $(this).toggleClass('pulse'); 
+});
+
+
+var hSong = document.getElementById ('hSong');
+var icon = document.getElementById ('icon');
+
+icon.onclick = function(){
+  if(hSong.paused){
+    hSong.play();
+    icon.src = "images/sound.png";
+  }
+  else{
+    hSong.pause();
+    icon.src = "images/mute.png";
+  }
+}
+
+
+function myFunction() {
+  var x = document.getElementById("hSong").loop;
+}
+
+
